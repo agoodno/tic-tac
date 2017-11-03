4e406d856c3a5272846658da1ed5f99c72e51cef
Andrew Goodnough <agoodno@gmail.com>
2017-11-03 11:15:52 -0500

    Can edit server now
    * Styled with bootstrap

--- src/Entry.js
+++ src/Entry.js
@@ -4,12 +4,13 @@ import 'Entry.css';
 class Entry extends Component {
   constructor(props) {
     super(props);
-    this.state = { query_template: '' };
+    this.state = { serverTemplate: '', queryTemplate: '' };
     this.environments = [{"code": "inhouse", "description":"Inhouse"},{"code":"prod", "description":"Production"}];
     this.handleEnvChange = this.handleEnvChange.bind(this);
+    this.handleServerTemplateChange = this.handleServerTemplateChange.bind(this);
     this.handleServerChange = this.handleServerChange.bind(this);
     this.handleQueryTemplateChange = this.handleQueryTemplateChange.bind(this);
-    this.handleQueryTextChange = this.handleQueryTextChange.bind(this);
+    this.handleQueryChange = this.handleQueryChange.bind(this);
     this.handleExecuteButtonClick = this.handleExecuteButtonClick.bind(this);
   }
 
@@ -17,85 +18,138 @@ class Entry extends Component {
     this.props.onEnvChange(evt.target.value);
   }
 
-  handleServerChange(evt) {
+  handleServerTemplateChange(evt) {
+    // console.log("handing server temp change"); console.log(evt.target.value);
+    this.setState({ serverTemplate: evt.target.value });
     var countyNo = evt.target.options[evt.target.selectedIndex].dataset.countyNo;
     this.props.onServerChange(evt.target.value, countyNo);
   }
 
-  handleQueryTemplateChange(evt) {
-    this.setState({ query_template: evt.target.value });
-    this.props.onQueryTextChange(evt.target.value);
+  handleServerChange(evt) {
+    // console.log("handing server change"); console.log(evt.target.value);
+    this.setState({ serverTemplate: '' });
+    this.props.onServerChange(evt.target.value, '');
   }
 
-  handleQueryTextChange(evt) {
-    this.setState({ query_template: '' });
-    this.props.onQueryTextChange(evt.target.value);
+  handleQueryTemplateChange(evt) {
+    // console.log("handing query temp change"); console.log(evt.target.value);
+    this.setState({ queryTemplate: evt.target.value });
+    this.props.onQueryChange(evt.target.value);
+  }
+
+  handleQueryChange(evt) {
+    this.setState({ queryTemplate: '' });
+    this.props.onQueryChange(evt.target.value);
   }
 
   handleExecuteButtonClick(evt) {
+    console.log("handling button click"); console.log(evt.target.value);
     this.props.onExecuteButtonClick(evt.target.value);
   }
 
   render() {
-    // console.log("Entry rendering with: "); console.log(this.state);
+    console.log("Entry rendering with: "); console.log(this.state);
 
-    var serverOptions = [<option key="-1" value="">--Select--</option>];
+    var serverOptions = [<option key="-1" value=""></option>];
     var envOptions = [];
     var queryOptions = [<option key="-1" value=""></option>];
 
-    this.props.servers.forEach((server, index) => {
+    this.environments.forEach((environment) => {
+      envOptions.push(<option key={environment.code} value={environment.code}>{environment.description}</option>);
+    });
+    this.props.serverTemplates.forEach((server, index) => {
       if (!server.disabled) {
         serverOptions.push(<option key={index} value={server.url} data-county-no={server.countyNo}>{server.name}</option>);
       }
     });
-    this.environments.forEach((environment) => {
-      envOptions.push(<option key={environment.code} value={environment.code}>{environment.description}</option>);
-    });
-    this.props.queries.forEach((query, index) => {
+    this.props.queryTemplates.forEach((query, index) => {
       queryOptions.push(<option key={index} value={query.text}>{query.name}</option>);
     });
 
     return (
       <div className="Entry">
-        <table>
-          <thead>
-            <tr>
-              <th>Environment</th>
-              <th>Servers</th>
-              <th>Query Templates</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <select value={this.state.environment} className="environments" onChange={this.handleEnvChange}>
-                  {envOptions}
-                </select>
-              </td>
-              <td>
-                <select value={this.state.server} className="servers" onChange={this.handleServerChange}>
-                  {serverOptions}
-                </select>
-              </td>
-              <td>
-                <select value={this.state.query_text} className="queries" onChange={this.handleQueryTemplateChange}>
-                  {queryOptions}
-                </select>
-              </td>
-            </tr>
-            <tr>
-              <td colSpan="3">
-                <textarea
-                  value={this.props.query_text}
-                  onChange={this.handleQueryTextChange}
-                  rows="5"
-                  cols="100"
-                  placeholder="Type your query here or select one of the common query templates."/>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button onClick={this.handleExecuteButtonClick}>Execute</button>
+        <div className="row">
+          <div className="form-group col-md-3">
+            <label>Environment</label>
+            <select
+               id="environments"
+               tabIndex="1"
+               value={this.props.environment}
+               className="environments form-control"
+               aria-describedby="environmentsHelp"
+               onChange={this.handleEnvChange}>
+              {envOptions}
+            </select>
+            <div>
+              <small id="environmentsHelp" className="form-text text-muted">Narrows the Servers and Query Templates choices.</small>
+            </div>
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="serverTemplates">Server Templates</label>
+            <select
+               id="serverTemplates"
+               value={this.state.serverTemplate}
+               className="servers form-control"
+               tabIndex="2"
+               aria-describedby="serverTemplatesHelp"
+               onChange={this.handleServerTemplateChange}>
+              {serverOptions}
+            </select>
+            <small id="serverTemplatesHelp" className="form-text text-muted">Pre-defined servers.</small>
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="server">Server</label>
+            <input id="server"
+                   type="text"
+                   value={this.props.server}
+                   className="form-control"
+                   tabIndex="0"
+                   aria-describedby="serverHelp"
+                   placeholder="Enter database url or select one from the dropdown"
+                   onChange={this.handleServerChange} />
+            <small id="serverHelp" className="form-text text-muted">Server to run the query against.</small>
+          </div>
+          <div className="form-group col-md-3">
+            <label htmlFor="countyNo">County No</label>
+            <input id="countyNo"
+                   type="text"
+                   value={this.props.countyNo}
+                   size="4"
+                   maxLength="2"
+                   className="form-control"
+                   tabIndex="0"
+                   aria-describedby="countyNoHelp"
+                   placeholder="Enter countyNo to use with this server" />
+            <small id="countyNoHelp" className="form-text text-muted">This will be substituted in wherever the $countyNo variable is found.</small>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="queryTemplates">Query Templates</label>
+          <select
+             id="queryTemplates"
+             value={this.state.queryTemplate}
+             className="queries form-control"
+             tabIndex="3"
+             onChange={this.handleQueryTemplateChange}>
+            {queryOptions}
+          </select>
+          <textarea
+             id="query"
+             className="form-control"
+             tabIndex="4"
+             value={this.props.query}
+             onChange={this.handleQueryChange}
+             aria-describedby="queryHelp"
+             rows="5"
+             cols="100"
+             placeholder="Enter your query here or select one of the common query templates."/>
+          <small id="queryHelp" className="form-text text-muted">Query to run. You can choose a canned query and then edit it in the text area.</small>
+        </div>
+        <button
+           id="execute"
+           className="btn btn-primary"
+           tabIndex="5"
+           onClick={this.handleExecuteButtonClick}>Execute</button>
       </div>
     );
   }
